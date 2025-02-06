37542ae764e47bc557c899686a62dfc6c23e2673
John Mark Tizado <johnmarktizado@gmail.com>
2025-02-07 02:08:41 +0800

    fix: simplify text color class in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,7 @@ export default function Footer() {
     <div className="text-primary-foreground flex justify-around px-4 bg-pink-100">
       <div className="flex flex-col items-center justify-center w-full max-w-screen-lg px-4 py-4 gap-4">
         <Image src="/mainLogo.png" alt="Logo" width={40} height={40} />
-        <p className="text-primary-foreground text-color  text-black ">
-          © 2025 SafeHeaven
-        </p>
+        <p className=" text-black">© 2025 SafeHeaven</p>
       </div>
     </div>
   );
