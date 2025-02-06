e31980eac9ddf7787d86ad2598c11aad93e62556
John Mark Tizado <johnmarktizado@gmail.com>
2025-02-06 12:15:58 +0800

    feat: implement order history email functionality and order deletion component

---
+++ src/email/OrderHistory.tsx
@@ -0,0 +1,81 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+import React from "react";
+
+type OrderHistoryEmailProps = {
+  orders: {
+    id: string;
+    pricePaidInCents: number;
+    createdAt: Date;
+    downloadVerificationId: string;
+    product: {
+      name: string;
+      imagePath: string;
+      description: string;
+    };
+  }[];
+};
+
+OrderHistoryEmail.PreviewProps = {
+  orders: [
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 10000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "test 1",
+        description: "HER gym wear",
+        imagePath:
+          "/products/0affff0b-75b5-44d4-83e8-f5fa79c14b9a-e-commerce her-gym-clothes 2.png",
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 2000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "test 2",
+        description: "just another product",
+        imagePath:
+          "/products/a295ba35-abfb-4ff5-bae0-ab1374d40dea-e-commerce-gymwear.png",
+      },
+    },
+  ],
+} satisfies OrderHistoryEmailProps;
+
+export default function OrderHistoryEmail({ orders }: OrderHistoryEmailProps) {
+  return (
+    <Html>
+      <Preview>Order History & Downloads</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Order History</Heading>
+            {orders.map((order, index) => (
+              <React.Fragment key={order.id}>
+                <OrderInformation
+                  order={order}
+                  product={order.product}
+                  downloadVerificationId={order.downloadVerificationId}
+                />
+                {index < orders.length - 1 && <Hr />}
+              </React.Fragment>
+            ))}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
