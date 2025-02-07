282958bd8af2277e3e331a01f2e25554451ed2e4
John Mark Tizado <johnmarktizado@gmail.com>
2025-02-08 01:25:09 +0800

    refactor: update Hero component layout and content for inclusivity and improved styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,22 +5,24 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="relative flex flex-col md:flex-row items-center bg-amber-100 w-full p-4 md:p-8 lg:h-dvh">
+    <div className="relative flex flex-col md:flex-row items-center bg-amber-100 w-full p-4 md:p-8 lg:h-dvh md:h-dvh h-96 sm:h-dvh justify-center">
       <div className="absolute inset-0 z-0">
         <Image
-          src="/heroBackground.jpg"
+          src="/Flags.png"
           alt="Hero Background"
           layout="fill"
           className="object-cover"
           priority
         />
       </div>
-      <div className="relative gap-2 gap-y-6 text-start md:text-start mb-4 md:mb-0 z-50 md:ml-8">
-        <h1 className="text-2xl md:text-4xl lg:text-6xl">Wellness for Her,</h1>
-        <h1 className="text-2xl md:text-4xl lg:text-6xl text-pink-500">
+      <div className="relative gap-2 gap-y-6 text-start lg:text-start md:text-start mb-4 md:mb-0 z-50 md:ml-8 text-wrap pb-28">
+        <h1 className="text-2xl md:text-3xl lg:text-5xl">
+          Wellness for Her/They/Them,
+        </h1>
+        <h1 className="text-2xl md:text-3xl lg:text-5xl text-pink-500">
           Wellness for Life.
         </h1>
-        <p className="text-base md:text-2xl mb-4">
+        <p className="text-sm sm:text-base md:text-xl lg:text-2xl mb-4">
           Your Hub for Health, Happiness, and Her Strength.
         </p>
         <Link href="/products">
