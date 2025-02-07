c04de6b71c0cff982e36ffe143fc384f7c1adb26
John Mark Tizado <johnmarktizado@gmail.com>
2025-02-08 01:24:06 +0800

    refactor: update Footer component layout and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="text-primary-foreground flex justify-around px-4 bg-pink-100">
-      <div className="flex flex-col items-center justify-center w-full max-w-screen-lg px-4 py-4 gap-4">
-        <Image src="/mainLogo.png" alt="Logo" width={40} height={40} />
-        <p className=" text-black">© 2025 SafeHeaven</p>
+    <div>
+      <hr className=" w-full border-gray-300" />
+      <div className="flex justify-center text-primary-foreground px-4 bg-pink-100 p-8">
+        <p className=" text-black">© 2025 SafeHaven</p>
       </div>
     </div>
   );
