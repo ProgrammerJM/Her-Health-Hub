07b3a24df7db85134932af58dd0ab9e93eda0bc2
John Mark Tizado <johnmarktizado@gmail.com>
2025-02-08 01:24:22 +0800

    first commit

---
+++ src/app/(customerFacing)/_components/OtherWebsites.tsx
@@ -0,0 +1,60 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function OtherWebsites() {
+  return (
+    <div className="grid lg:grid-cols-2 justify-items-center gap-8 p-12">
+      <Card>
+        <CardHeader>
+          <h1 className="font-bold text-3xl text-black">MENSTRUAL CYCLE</h1>
+        </CardHeader>
+        <CardContent className="grid grid-col-2 sm:grid-col md:grid-col lg:grid-row  gap-4">
+          <Image
+            src="/MenstrualCycle.jpg"
+            alt="Menstrual Cycle"
+            width={400}
+            height={400}
+          />
+          <p className="text-wrap">
+            Being able to predict when your next period will arrive can save you
+            a lot of hassle and help you to understand your own menstrual cycle.
+            Predict your next period now with Flo&apos;s easy-to-use period
+            calculator.{" "}
+            <Link
+              href="https://flo.health/tools/menstrual-cycle-calculator"
+              className="text-pink-400"
+            >
+              Learn more
+            </Link>
+          </p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <h1 className="font-bold text-3xl text-black">SEXUAL HEALTH</h1>
+        </CardHeader>
+        <CardContent className="grid grid-col-2 sm:grid-col md:grid-col lg:grid-row gap-4">
+          <Image
+            src="/SexualHealth.png"
+            alt="Sexual Health"
+            width={300}
+            height={300}
+          />
+          <p>
+            Sexual health is an important part of overall health. Understanding
+            your sexual health can help you make informed decisions about your
+            body and your relationships. .{" "}
+            <Link
+              href="https://loveyourself.ph/hiv-prevention/"
+              className="text-pink-400"
+            >
+              Learn more
+            </Link>
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
---
+++ src/app/(customerFacing)/_components/RelevantSites.tsx
@@ -0,0 +1,47 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import OtherWebsites from "./OtherWebsites";
+
+export default function RelevantSites() {
+  return (
+    <>
+      <OtherWebsites />
+      <Card className="bg-pink-100 text-white">
+        <CardHeader className="flex items-center justify-center">
+          <div className="bg-pink-300 w-fit p-4 rounded-lg ">
+            <h1 className="font-bold text-3xl text-black">
+              HISTORY OF PRIDE MONTH
+            </h1>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="grid lg:grid-cols-2 gap-8 space-x-4 items-center justify-items-center">
+            <div className="bg-gray-300 rounded-xl">
+              <p className="text-wrap font-semibold text-lg p-8 text-black">
+                The origins of Pride Month trace back to a riot in June 1969, a
+                pivotal moment in LGBTQ+ history. These riotsmarked the
+                beginning of a more assertive and organized effort to fight for
+                LGBTQ+ rights. The following year, the first Pride March was
+                held, commemorating the anniversary of the Stonewall Uprising
+                and setting the stage for annual Pride celebrations around the
+                world.
+              </p>
+            </div>
+            <div className="relative w-96 h-96">
+              <Link href="https://loveyourself.ph/">
+                <Image
+                  src={"/pride.png"}
+                  width={600}
+                  height={800}
+                  alt="Trans Banner"
+                />
+              </Link>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
